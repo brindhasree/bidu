382ab3377bb05802a3d80ab22d7b7987eb6a5bcf
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-05 17:17:42 +0530

    Create largestdivisorof2.c

---
+++ largestdivisorof2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i,l=1;
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+    
+        if(n1%i==0 && n2%i==0)
+        {
+        if(i>l)
+            l= i;
+        }
+    }
+
+    printf("%d",l);
+
+    return 0;
+}

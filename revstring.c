3a95e4beac9170acae1668c3c3207e4a33e549ae
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-07 18:01:32 +0530

    Update and rename avoidvowelsandrev1.c to revstring.c

---
+++ revstring.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+	char str[20];
+	int len, i, j;
+	printf("Enter a string : ");
+	scanf("%s",str);
+	len=strlen(str);
+	for(i=0; i<len; i++)
+	{
+		if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+		str[i]=='o' || str[i]=='u' || str[i]=='A' ||
+		str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+		str[i]=='U')
+		{
+			for(j=i; j<len; j++)
+			{
+				str[j]=str[j+1];
+			}
+		len--;
+		}
+	
+		
+	}
+	
+	
+	for(i=len-1;i>0;i--)
+	{
+		printf("%s",str);
+	}
+  //qqq
+	return 0;
+
+}
